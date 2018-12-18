886dce7135134b5872d7cc1c67b4ebb3cad2b53b
Jack <jackrawlinson@live.co.uk>
2018-12-18 11:57:32 +0000

    Implemented redirects. Removed hashid column from database

--- public/index.php
+++ public/index.php
@@ -6,26 +6,33 @@ require_once __DIR__ . '/../src/util.php';
 // Default index page
 router('GET', '^/$', function() {
     $short_url = new ShortUrlController();
-    $results = $short_url->read();
-
+    $results = $short_url->get();
     http_response_code(200);
-    echo 'Get' . json_encode($results);
+    echo json_encode($results, JSON_UNESCAPED_SLASHES);
 });
 
 router('GET', '^/(?<hash>.+)$', function($params) {
-    echo $_SERVER['SERVER_NAME'];
-    echo "Hash: ";
-    var_dump($params);
+    $hashid = $params["hash"];
+    $urlcontroller = new ShortUrlController();
+
+    if(strlen($hashid) === 8) {
+        $url = $urlcontroller->getForHashId($hashid);
+        header('Location: '.$url, true, 302);
+        die();
+    } else {
+        http_response_code(404);
+        echo json_encode(array("message" => "Not Found"));
+    }
 });
 
 router('POST', '^/shorten$', function() {
     $post = file_get_contents('php://input');
     $json = json_decode($post);
-    $short_url = new ShortUrlController();
+    $urlcontroller = new ShortUrlController();
     $host = $_SERVER['HTTP_HOST'];
 
     if(!empty($json->url) && filter_var($json->url, FILTER_VALIDATE_URL) !== false)  {
-        $result = $short_url->shorten($json->url, $host);
+        $result = $urlcontroller->shorten($json->url, $host);
         if(!is_null($result)) {
             http_response_code(201);
             echo json_encode(array("url" => $result), JSON_UNESCAPED_SLASHES);
--- src/ShortUrlController.php
+++ src/ShortUrlController.php
@@ -1,4 +1,3 @@
-
 <?php
 require_once dirname(__FILE__) . '/../bootstrap.php';
 require_once dirname(__FILE__) . '/./DBManager.php';
@@ -11,20 +10,19 @@ use Hashids\Hashids;
 class ShortUrlController
 {
     private $connection;
-    private $table_name = "shorturl";
+    private $hashids;
 
     public function __construct(){
         $db_manager = new DBManager();
         $this->connection = $db_manager->getConnection();
         if ($this->connection->connect_error) {
-            die("Connection failed: " . $this->connection->connect_error);
+            die("Cannot connect to Database: " . $this->connection->connect_error);
         }
+        $this->hashids = new Hashids('', 8);
     }
 
-    // read products
-    function read(){
+    function get(){
         $data = null;
-
         $query = "SELECT * FROM shorturl";
         $statement = $this->connection->prepare($query);
 
@@ -37,31 +35,51 @@ class ShortUrlController
         return "Failed";
     }
 
+    function getForHashId($hashid) {
+        $id = $this->hashids->decode($hashid)[0];
+        $data = [ 'id' => $id ];
+        $statement = $this->connection->prepare("SELECT * FROM shorturl WHERE id=:id");
+
+        if($statement->execute($data)) {
+            $result = $statement->fetch(PDO::FETCH_ASSOC);
+            if($result !== null) {
+                $this->increment($result['id'], $result['visit_count']);
+                return $result['url'];
+            }
+        } else {
+            echo "Error: " . var_dump($statement) . "<br>" . $this->connection->error;
+        }
+        return null;
+    }
+
     function shorten($url, $host) {
         $data = [ 'url' => $url];
         $statement = $this->connection->prepare("INSERT INTO shorturl (url) VALUES (:url)");
 
         if($statement->execute($data)) {
             $last = $this->connection->lastInsertId();
-
-            $hashids = new Hashids('', 8);
-            $hashid = $hashids->encode($last);
-
-            $update_data = [
-                'hashid' => $hashid,
-                'id' => $last
-            ];
-            $update_statement = $this->connection->prepare("UPDATE shorturl SET hashid=:hashid WHERE id=:id");
-            if($update_statement->execute($update_data)) {
-                return "http://" . $host . "/" .  $hashid;
-            } else {
-                echo "Error: " . $update_statement . "<br>" . $this->connection->error;
-            }
+            $hashid = $this->hashids->encode($last);
+            return "http://" . $host . "/" .  $hashid;
         } else {
-            echo "Error: " . $statement . "<br>" . $this->connection->error;
+            echo "Error: " . var_dump($statement). "<br>" . $this->connection->error;
         }
-
         return null;
+    }
+
+    /**
+     * Increments the visited count of a url. Basic usage tracking of created URL's
+     * @param $id
+     * @param $count
+     */
+    private function increment($id, $count) {
+        $data = [
+            'id' => $id,
+            'visit_count' => $count + 1
+        ];
+        $statement = $this->connection->prepare("UPDATE shorturl SET visit_count=:visit_count WHERE id=:id");
+        if(!$statement->execute($data)) {
+            echo "Failed to increment counter" . "<br>" . $this->connection->error;
+        }
     }
 
 }
